aced8c8ae484ae0bbff86cc18873621cae17e5fe
Alexey Scorp <lezard821989@yandex.ru>
2023-04-17 15:55:22 +0700

    Prettier debag

--- src/views/layouts/posts/posts.jsx
+++ src/views/layouts/posts/posts.jsx
@@ -1,34 +1,35 @@
 import React from 'react';
 
 const Posts = () => {
-return (
-<div id="posts_wrap">
-<div id="posts_box">
-<div id="post_content">
-<div className="main_post">
-<div className="post_logo">
-<h2>Lorem ipsum dolor sit amet</h2>
-</div>
-<div className="post_discription">
-<span>
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque officia asperiores impedit perferendis! Accusantium, commodi!
-</span>
-</div>
-<div className="post_image"></div>
-<div className="post_main_date">
-<h3>25</h3>
-<p>April</p>
-</div>
-</div>
-<div className="next_post">
-<div className="post_discription add_post"></div>
-<div className="post_image add_post"></div>
-</div>
-</div>
-<div id="post_navigation"></div>
-</div>
-</div>
-)
-}
+  return (
+    <div id="posts_wrap">
+      <div id="posts_box">
+        <div id="post_content">
+          <div className="main_post">
+            <div className="post_logo">
+              <h2>Lorem ipsum dolor sit amet</h2>
+            </div>
+            <div className="post_discription">
+              <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+                officia asperiores impedit perferendis! Accusantium, commodi!
+              </span>
+            </div>
+            <div className="post_image"></div>
+            <div className="post_main_date">
+              <h3>25</h3>
+              <p>April</p>
+            </div>
+          </div>
+          <div className="next_post">
+            <div className="post_discription add_post"></div>
+            <div className="post_image add_post"></div>
+          </div>
+        </div>
+        <div id="post_navigation"></div>
+      </div>
+    </div>
+  );
+};
 
 export default Posts;
